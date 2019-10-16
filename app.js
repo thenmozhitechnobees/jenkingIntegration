f08616da97cff23c9ac99038fd0eb8c88a6c9d01
Thenmozhi Govindasamy <thenmozhi.technobees@gmail.com>
2019-10-16 16:22:23 +0530

    Sample API's has been added

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/',function(req,res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get('/getSampleData',function(req,res) {
   res.send("Hello World");
 });
 
